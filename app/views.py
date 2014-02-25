da4daaff04a22f9b8b63f851ad93cfd65cb8bc65
Carl Shotwell <carl.shotwell@leandog.com>
2014-02-25 00:56:48 -0500

    restructuring

--- app/views.py
+++ app/views.py
@@ -1,12 +1,7 @@
+from app import app
 from flask import Flask
 from flask import render_template
-app = Flask(__name__)
 
 @app.route('/')
 def hello_world(name=None):
     return render_template('sample.html', name=name)
-
-
-if __name__ == '__main__':
-    app.run()
-
