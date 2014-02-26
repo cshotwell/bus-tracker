72a66b321888d7e38ef0bdf42bfa3cf5eaef43bd
Carl Shotwell <carl.shotwell@leandog.com>
2014-02-25 23:52:18 -0500

    updating model, and fetching scripts

--- app/models.py
+++ app/models.py
@@ -5,10 +5,21 @@ ROLE_ADMIN = 1
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(64), index = True, unique = True)
-    photo_url = db.Column(db.String(120), index = True, unique = True)
+    username = db.Column(db.String(64), unique = True)
+    photo_url = db.Column(db.String(120), unique = True)
+    bus = db.Column(db.String(50))
     role = db.Column(db.SmallInteger, default = ROLE_USER)
+    tweets = db.relationship('Tweet', backref = 'author', lazy = 'dynamic')
 
     def __repr__(self):
-        return '<User %r>' % (self.nickname)
+        return '<User %r>' % (self.username)
+
+class Tweet(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(300))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % (self.body)
 
