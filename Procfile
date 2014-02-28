185ce85c63c2f4b035b008b7c22197bef3f031cd
Carl Shotwell <carl.shotwell@leandog.com>
2014-02-27 22:33:54 -0500

    updating database creation scripts

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: gunicorn run:app
+create_db: python create_db.py
 init: python db_create.py && python fetch_tweets.py
---
+++ create_db.py
@@ -0,0 +1,13 @@
+from migrate.versioning import api
+from sqlalchemy import create_engine
+from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+from app import db
+import os.path
+
+engine = create_engine('postgres://postgres@/postgres')
+conn = engine.connect()
+conn.execute("commit")
+conn.execute("create database startup_bus")
+conn.close()
+
