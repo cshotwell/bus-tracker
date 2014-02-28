48aa4d3e7eb5db18ec5d9d2459eb2f4ec5281a2c
Carl Shotwell <carl.shotwell@leandog.com>
2014-02-27 20:02:34 -0500

    updating it again

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ if os.environ.get('DATABASE_URL') is None:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db') + '?check_same_thread=False'
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_RECORD_QUERIES = True
+
 
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 #SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
