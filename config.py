21e7f68b5612cb25f925626b3cbef8f20d61c43c
Carl Shotwell <carl.shotwell@leandog.com>
2014-02-27 19:59:36 -0500

    updating config

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db') + '?check_same_thread=False'
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+#SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 CONSUMER_KEY = 'pdbdJxVaKClslVV3YHKWQ'
 CONSUMER_SECRET = 'Sa3ETL5ICForVs7UfkqcIERtLHljwWs3Boy6Y11K2aw'
