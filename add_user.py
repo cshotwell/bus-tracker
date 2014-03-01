2bf3b9b6c9845c86869aaf4001bfea22f758f4c3
Carl Shotwell <carl.shotwell@leandog.com>
2014-03-01 15:06:32 -0500

    adding script to add user without tweets

---
+++ add_user.py
@@ -0,0 +1,17 @@
+from app import db
+from app import models
+from sys import argv
+
+def add_user(username, bus):
+    user = models.User.query.filter_by(username=username).first()
+    if not user: #if there is no user, save this one
+        user = models.User(username=username, bus=bus, role=models.ROLE_USER)
+        db.session.add(user)
+        db.session.commit()
+
+
+if __name__ == '__main__':
+    username = argv[1]
+    bus = argv[2]
+    add_user(username, bus)
+
